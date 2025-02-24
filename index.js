090f676c79c5837d93eb4b2eeabae3b8693832a7
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2025-02-24 23:29:33 +0530

    add user registration endpoint with validation and duplicate check

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const config = require("./config.json");
 
 mongoose.connect(config.connectionString);
 
+const User = require("./models/user.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -29,8 +31,36 @@ app.post("/user", async (req, res) => {
   const { fullName, email, password } = req.body;
 
   if (!fullName) {
-    return res.status(400).json({ error: "Full Name is required" });
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required" });
   }
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "User already exists",
+    });
+  }
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
 });
 
 app.listen(8000);
